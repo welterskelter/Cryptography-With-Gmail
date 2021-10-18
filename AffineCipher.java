be602c31633e53edde2c69e033eac7d603f071b1
welterskelter <92701682+welterskelter@users.noreply.github.com>
2021-10-18 10:01:53 -0500

    Update AffineCipher.java

--- AffineCipher.java
+++ AffineCipher.java
@@ -2,11 +2,15 @@
 public class AffineCipher {
 	private int a,b,aInverse,m;
 	private int[] encryptionTable = new int[26];
+	private int[] decryptionTable = new int[26];
 	public AffineCipher(int a, int b) {
 		this.a = a;
 		this.b = b;
 		this.m = 26;
 		generateEncryptionTable();
+		generateInverseA();
+		generateDecryptionTable();
+		
 	}
 	
 	private void generateEncryptionTable() {
@@ -14,12 +18,14 @@ public class AffineCipher {
 			encryptionTable[i] = (a*i+b)%m;
 		}
 	}
-	public int generateInverseA() {
-		 int m0 = m;
-	        int y = 0, x = 1;
-	 
+	private void generateInverseA() {
+		//thank you geeks for geeks for this handy algo
+		int m0 = m;
+	        int y = 0; 
+		aInverse = 1;
+		 /*not needed since m always equals 26
 	        if (m == 1)
-	            return 0;
+	            return 0;*/
 	 
 	        while (a > 1) {
 	            // q is quotient
@@ -33,17 +39,21 @@ public class AffineCipher {
 	            a = t;
 	            t = y;
 	 
-	            // Update x and y
-	            y = x - q * y;
-	            x = t;
+	            // Update aInverse and y
+	            y = aInverse - q * y;
+	            aInverse = t;
 	        }
 	 
-	        // Make x positive
-	        if (x < 0)
-	            x += m0;
-	 
-	        return x;
+	        // Make aInverse positive
+	        if (aInverse < 0)
+	            aInverse += m0;
+		System.out.println(aInverse);
 	    }
+	private void generateDecryptionTable(){
+		for(int i = 0;i<m;i++){
+			decryptionTable[i] = ((i-b)*aInverse)%m;
+		}
+	}
 	 
 	
 	/*public String encrypt(String line) {
